381e86418001bd97b6b2782ee0603a6874f7a8ed
Verg82 <verg82@mail.ru>
2022-06-12 20:53:29 +0800

    Initial commit

---
+++ src/ru/netology/Main.java
@@ -0,0 +1,9 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("10011", "10011"));
+        System.out.println(bins.mult("10011", "10011"));
+    }
+}
